2d6d6e67f444062b1c4daf67395a9ace8943ef06
maksimart <145654202+maksimart@users.noreply.github.com>
2023-10-04 23:40:41 +0300

    Add files via upload

---
+++ dz1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //%1
+    int t;
+    cout << "how many seconds?" << endl;
+    cin >> t;
+    cout << t / 3600 << " hours " << (t % 3600 / 60) << " minuts " << (t % 3600 % 60) << " seconds";
+
+    //%2
+    float money, b;
+    int a;
+    cout << "enter a monetary unit" << endl;
+    cin >> money;
+    a = money;
+    b = (money - a) * 100;
+    cout << a << " rubles " << b << " kopecks ";
+
+    //%3
+    int day1,week,day2;
+    cout << "quantity day" << endl;
+    cin>>day1;
+    week = day1 / 7;
+    day2 = day1 % 7;
+    cout << "week:" << week << "  day:" << day2;
+}
