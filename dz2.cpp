54c30a7b945a502cd31e9e87e9726acaf64e1130
maksim <145654202+maksimart@users.noreply.github.com>
2023-10-05 00:53:24 +0300

    Add files via upload

---
+++ dz2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	//№1
+	//int nam1,digit1,digit2,cdigit3,digit3,digit4,digit5,digit6,sum1,sum2;
+	//cout << "enter number:";
+	//cin >> nam1;
+	//if (nam1 >=100000 && nam1<1000000) {
+	//	digit1 = nam1 / 100000;
+	//	digit2 = nam1 / 10000 % 10;
+	//	digit3 = nam1 / 1000 % 10;
+	//	digit4 = nam1 / 100 % 10;
+	//	digit5 = nam1 / 10 % 10;
+	//	digit6 = nam1 % 10;
+	//	sum1 = digit1 + digit2 + digit3;
+	//	sum2 = digit4 + digit5 + digit6;
+	//	if (sum1 == sum2)cout << "lucky";
+	//	else cout << "unlucky";
+	//}
+	//else cout << "error";
+
+	//№2
+	//int nam1,digit1,digit2,digit3,digit4;
+	//cout << "enter number:";
+	//cin >> nam1;
+	//digit1 = nam1 / 1000 % 10;
+	//digit2 = nam1 / 100 % 10;
+	//digit3 = nam1 / 10 % 10;
+	//digit4 = nam1 % 10;
+	//nam1 = (digit2 * 1000) + (digit1 * 100) + (digit4 * 10) + digit3;
+	//cout << nam1;
+
+	//№3
+	int max,digit1, digit2, digit3, digit4, digit5, digit6, digit7;
+	cin >> digit1 >> digit2 >> digit3 >> digit4 >> digit5 >> digit6 >> digit7;
+	max = 0;
+	if (max < digit1)max = digit1;
+	if (max < digit2)max = digit2;
+	if (max < digit3)max = digit3;
+	if (max < digit4)max = digit4;
+	if (max < digit5)max = digit5;
+	if (max < digit6)max = digit6;
+	if (max < digit7)max = digit7;
+	cout << max;
+}
